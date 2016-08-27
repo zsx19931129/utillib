b7764eb30952a5fa41e36bc238206d4cbb9205b1
shaoxiong <shaoxiong@splunk.com>
2016-08-27 23:13:33 +0800

    init cpputillib project

---
+++ CppUtilFunctions/CppUtilFunctions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppUtilFunctions
+//
+//  Created by shaoxiong on 8/27/16.
+//  Copyright © 2016 shaoxiong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
